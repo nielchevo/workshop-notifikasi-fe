f034bb264a648d30764df2232de16f4077beae02
Nathaniel Gunawan <nielchvo@gmail.com>
2022-04-05 22:16:34 +0700

    feat: add oneSignal sdk worker for browser

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+        OneSignal.init({
+            appId: "9462ed27-fa87-429e-95a2-f14466a0078d",
+            safari_web_id: "web.onesignal.auto.13f7d09c-87f4-478e-9a86-b96c3b883b5b",
+            notifyButton: {
+                enable: true,
+            },
+
+            allowLocalhostAsSecureOrigin: true,
+        });
+    });
+
+    return () => {
+        window.OneSignal = undefined;
+    };
+  }, []); // <-- run this effect once on mount
+
   return <Component {...pageProps} />
 }
 
